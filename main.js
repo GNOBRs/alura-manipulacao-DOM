15a3b5fd176a149bfcbe185081eced7e7e23b0d2
Bruno Bossois Hohmann <gnobr1@gmail.com>
2021-12-28 14:52:51 -0300

    Corrige erros de sintaxe na criação do parágrafo, mudança no reset do valor de input, apaga classes repetidas no CSS ,nova verificação de input vazio e sua mensagem de erro no console junto de seu parágrafo no HTML e classe no CSS

--- main.js
+++ main.js
@@ -3,22 +3,33 @@ import BotaoDeleta from "./componentes/deletaTarefa.js";
 
 const criarTarefa = (evento) => {
 	evento.preventDefault();
-
-	const lista = document.querySelector("[data-list]");
+	
 	const input = document.querySelector("[data-form-input]");
 	const valor = input.value;
 
-	const tarefa = document.createElement("li");
-	tarefa.classList.add("task");
+	if (valor != "") {
+		const textoError = document.querySelector("[data-invalid-input]");
+		textoError.classList.remove("invalido");
 
-	const conteudo = `<p class=content>${valor}</p> </p>`;
+		const lista = document.querySelector("[data-list]");
 
-	tarefa.innerHTML = conteudo;
-	tarefa.appendChild(BotaoConclui());
-	tarefa.appendChild(BotaoDeleta());
-	lista.appendChild(tarefa);
+		const tarefa = document.createElement("li");
+		tarefa.classList.add("task");
 
-	input.value = " ";
+		const conteudo = `<p class=content>${valor}</p>`;
+
+		tarefa.innerHTML = conteudo;
+		tarefa.appendChild(BotaoConclui());
+		tarefa.appendChild(BotaoDeleta());
+		lista.appendChild(tarefa);
+
+		input.value = null;
+	} else {
+		const textoError = document.querySelector("[data-invalid-input]");
+		textoError.classList.add("invalido");
+
+		throw new Error("Favor definir valor no input");
+	}
 };
 
 const novaTarefa = document.querySelector("[data-form-button]");
